3448edf2e8a323b10836677389d17778ce52c704
nite613 <nite613>
2004-09-29 04:04:48 +0000

    oops, missed new file from patch

---
+++ waste/resources.hpp
@@ -0,0 +1,30 @@
+/*
+WASTE - resources.hpp (Simple wrapper to switch between server and client)
+Copyright (C) 2003 Nullsoft, Inc.
+Copyright (C) 2004 WASTE Development Team
+
+WASTE is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+WASTE  is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WASTE; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef _RESOURCES_H_
+#define _RESOURCES_H_
+
+#ifdef _DEFINE_SRV
+	#include "resourcesrv.hpp"
+#else
+	#include "resource.hpp"
+#endif
+
+#endif // _RESOURCES_H_
