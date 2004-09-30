cdd027a4e6357224ab0d1fe224552617da36f5df
gvdl <gvdl>
2004-09-30 04:19:50 +0000

    RID-1035747: Move "stdafe.hpp" into platform.hpp and clean up references

--- waste/asyncdns.cpp
+++ waste/asyncdns.cpp
@@ -18,7 +18,6 @@ along with WASTE; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include "stdafx.hpp"
 #include "platform.hpp"
 #include "sockets.hpp"
 #include "asyncdns.hpp"
