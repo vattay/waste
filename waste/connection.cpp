0afd74ca27bad3e7274c7e71b38a790ea58ef34b
nite613 <nite613>
2004-09-30 20:26:49 +0000

    Segfault problem with dns invalidation fixed

--- waste/connection.cpp
+++ waste/connection.cpp
@@ -433,8 +433,8 @@ C_Connection::~C_Connection()
 	m_fish_send.Final();
 	m_fish_recv.Final();
 	memset(m_mykeyinfo.raw,0,sizeof(m_mykeyinfo.raw));
-	//If we're dying and we never successfully connected, invalidate DNS cache entries for this host
-	if(!m_ever_connected)
+	//If an outbound connection is dying and we never successfully connected, invalidate DNS cache entries for this host
+	if(m_host && !m_ever_connected)
 		m_dns->invalidate(m_host);
 	else{
 		char descstr_s[16+SHA_OUTSIZE*2+32];
